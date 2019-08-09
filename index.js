74778d5c158eb43accb6a2f4cc8a1795cb6f7734
nataliaev <nataliiaevchenko@gmail.com>
2019-08-09 10:42:30 +0200

    Enable sse

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const Sse = require('json-sse')
+const bodyParser = require('body-parser')
+
+const data = 'hello world'
+const sse = new Sse(data)
 
 const app = express()
+
+const parserMiddleware = bodyParser.json()
+app.use(parserMiddleware)
+
+app.get('/stream', sse.init)
+app.post('/message', (request, response) => {
+  const { message } = request.body
+  sse.send(message)
+  response.send(message)
+})
 
 const port = process.env.PORT || 5000
 
