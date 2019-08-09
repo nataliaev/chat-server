6e86a384642f915ef9bedd1b80f7b6bf6a85c88f
nataliaev <nataliiaevchenko@gmail.com>
2019-08-09 17:40:04 +0200

    Add channel

--- index.js
+++ index.js
@@ -9,17 +9,21 @@ const databaseUrl =
   "postgres://postgres:secret@localhost:5432/postgres";
 const db = new Sequelize(databaseUrl);
 
-db
-  .sync({ force: false })
-  .then(() => {
+db.sync({ force: false }).then(() => {
   console.log("Database synced");
 });
 
-const Message = db.define(
-  "message", {
-    text: Sequelize.STRING,
-    user: Sequelize.STRING
+const Message = db.define("message", {
+  text: Sequelize.STRING,
+  user: Sequelize.STRING
 });
+
+const Channel = db.define("channel", {
+  name: Sequelize.STRING
+});
+
+Message.belongsTo(Channel);
+Channel.hasMany(Message);
 
 const sse = new Sse();
 
@@ -32,24 +36,41 @@ const parserMiddleware = bodyParser.json();
 app.use(parserMiddleware);
 
 app.get("/stream", async (request, response) => {
-  const messages = await Message.findAll();
+  const channels = await Channel.findAll({ include: [Message] });
 
-  const data = JSON.stringify(messages);
+  const data = JSON.stringify(channels);
   sse.updateInit(data);
 
   sse.init(request, response);
 });
 
-app.post(
-  "/message", 
-  async (request, response) => {
-  const { message, user } = request.body;
+app.post("/channel", async (request, response) => {
+  const channel = await Channel.create(request.body);
 
-  const entity = await Message.create({ text: message, user });
+  const channels = await Channel.findAll({
+    include: [Message]
+  });
 
-  const messages = await Message.findAll();
+  const data = JSON.stringify(channels);
 
-  const data = JSON.stringify(messages);
+  sse.updateInit(data);
+  sse.send(data);
+
+  response.send(channel);
+});
+
+app.post("/message", async (request, response) => {
+  const { message, user, channelId } = request.body;
+
+  const entity = await Message.create({
+    text: message,
+    user,
+    channelId
+  });
+
+  const channels = await Channel.findAll({ include: [Message] });
+
+  const data = JSON.stringify(channels);
 
   sse.updateInit(data);
   sse.send(data);
